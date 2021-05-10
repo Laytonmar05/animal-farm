5b675ad8e854e187facddba8138df3ffdf556020
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-05-10 09:24:35 -0700

    some holy grail

--- script/app.js
+++ script/app.js
@@ -1,38 +1,27 @@
-'use strict'
+//console.log('hello world');
 
-// 1. Targeted the form. - put this aside for later
-const form = document.getElementsByTagName('FORM')[0];
+const form = document.getElementsByTagName('FORM') [0];
 
-// 2. Target the text box - input of "title". get its value
+form.addEventListener('submit', makeNewArticle);
 
-// 3. Target the text area - textarea and get its value
-
-// 4. Add an event listener to the ?? (form or the submit button)
-
-// 5. create a function and pass that function to the event listener
-
-form.addEventListener('submit', makeNewArticle ); // 
-
-// When you invoke a function later... like when the button is clicked, what is the function called? ---> callback function
 
 
 function makeNewArticle(event) {
- // 6. how to respond to the click
-event.preventDefault();
-const title = event.target.title.value;
-const journal = event.target.content.value;
+    event.preventDefault();
+    const title = event.target.title.value;
+    const journal = event.target.content.value;
 
-const article = document.createElement('article');
-const h2 = document.createElement('h2');
-const p = document.createElement('p');
+    const article = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
 
-h2.textContent = title;
-p.textContent = journal;
 
-article.appendChild(h2);
-article.appendChild(p);
+    h2.textContent = title;
+    p.textContent = journal;
 
-const main = document.querySelector('main')
-main.appendChild(article);
+    article.appendChild(h2);
+    article.appendChild(p);
 
+    const main = document.querySelector('main')
+    main.appendChild(article);
 }
--- script/script.js
+++ script/script.js
@@ -1,55 +1,57 @@
-'use strict'
-
-// Create a constructor function as a template for animals I want to create
-// constructor takes one parameter of animalName and assigns it to the property animalName
 function Animal(animalName) {
   this.animalName = animalName;
 }
+//this function is like the constructor function for the animals that we will create. It is just the overall template for the objects that will be made. 
 
-// render tells the specific animal to put itself on the screen
-Animal.prototype.render = function() {
-  console.log("this function has been called")
-  // what does this declaration do?
-  let animalDiv = document.createElement('div');
-  // what is this next line doing?
-  animalDiv.className = `${this.animalName}Object`;
-  // what is this next line doing?
-  animalDiv.textContent = this.animalName;
-  let animalSection = document.querySelector(`#${this.animalName}Section`)
-  animalSection.appendChild(animalDiv);
+
+Animal.prototype.render = function() { //this render function is like the template for all animals to be put on the page. When it is called the animals information will be printed onto the website. 
+
+let animalDiv = document.createElement('div');
+animalDiv.className = `${this.animalName}Object`;
+animalDiv.textContent = this.animalName;
+
+let animalSection = document.querySelector(`#${this.animalName}Section`);
+
+animalSection.appendChild(animalDiv);
 }
+//this function is the render function. It contains evrything that will be rendered onto the webpage. This function is responsible for 
+//printing everything on the screen. 
 
-//make cat
 let makeCatButton = document.getElementById('makeCat');
-makeCatButton.addEventListener('click', goMakeCat)
-function goMakeCat() {
-  let cat = new Animal('cat');
-  cat.render();
+makeCatButton.addEventListener('click', respond)
+  
+function respond() { {
+
+let cat = new Animal('cat')
+cat.render(); //this render is specific to the cat, this is saying the cat will be rendered on the screen. 
 }
-//make dog
+
+//This function is the function that tells the computer what to print (which animal and its information ) onto the screen when the button is clicked. 
+
+
+
+
+}
 let makeDogButton = document.getElementById('makeDog');
-makeDogButton.addEventListener('click', goMakeDog)
+makeDogButton.addEventListener ('click',goMakeDog)
+
 function goMakeDog() {
   let dog = new Animal('dog');
-  dog.render();
-}
+  dog.render();// The dog will be rendered on the screen. (when the button is pushed)
+}// the most specific declarations to the render is that the the animals will only be rendered on the page when that button is clicked. 
+//All the last 4 functions are responsible for printing the correct animal on the screen when that button is clicked. These functions are only invoked when 
+//the button(s) are clicked. 
+let makeHorseButton = document.getElementById('makeHorse');// this conencts the button for horse to the id in the html. 
+makeHorseButton.addEventListener ('click',goMakeHorse)//tells the computer what to do when the makeHorse button is clicked. 
 
-// make horse
-
-let makeHorseButton = document.getElementById('makeHorse');
-makeHorseButton.addEventListener('click', goMakeHorse)
 function goMakeHorse() {
-  let horse = new Animal('horse');
-  horse.render();
+  let horse = new Animal('horse');//creating a new animal. this is possible because of the constructir function and its template. 
+  horse.render();// The horse will be rendered on the screen when the button is pushed. 
 }
-
-// make sheep
-
 let makeSheepButton = document.getElementById('makeSheep');
-makeSheepButton.addEventListener('click', goMakeSheep)
+makeSheepButton.addEventListener ('click',goMakeSheep)
 
 function goMakeSheep() {
-  let sheep = new Animal('sheep');
-  sheep.render();
+  let sheep= new Animal('sheep');
+  sheep.render();// The sheep will be rendered on the screen when the button is pushed. 
 }
-
